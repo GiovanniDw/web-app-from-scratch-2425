1d2e1478a026fabca81974a1de0cea649ad6b077
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2025-02-11 16:41:42 +0100

    add loading

---
+++ src/loading.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    document.getElementById('flip-card').classList.add('is-flipped');
+  }, 2000); // Flip na 2 seconden
+});

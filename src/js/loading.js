4bab5183683c6b18223969d3d423931cba228c28
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2025-02-13 10:50:51 +0100

    fix icon colors & remove unused script

--- src/js/loading.js
+++
@@ -1,9 +0,0 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//   setTimeout(() => {
-
-//     if (document.getElementById('flip-card')) {
-//       document.getElementById('flip-card').classList.add('is-flipped');
-// }
-  
-//   }, 2000); // Flip na 2 seconden
-// });
